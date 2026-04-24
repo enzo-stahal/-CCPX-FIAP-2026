daf4df442dfb90fb18015dcc55ec3bf0c3976656
enzo-stahal <enzo.stahal@gmail.com>
2026-04-24 19:26:34 -0300

    aula04

---
+++ aula04_loop_de_repeticao/aula04-while.py
@@ -0,0 +1,27 @@
+cp = 0
+while cp < 3:
+    print(f"Produto {cp}")
+    cp += 1
+    
+# # while decrescente de 4 ate 1
+i = 4
+while i > 0:
+    print(i)
+    i -=1
+
+
+# Repetição com entrada de usuario
+jogar = "sim"
+
+while jogar.lower() == "sim":
+    print("Iniciar ou repetir o jogo")
+
+    jogar = input("Deseja jogar novamente?: ")
+
+i = 0
+while i < 10:
+    i += 1
+
+    if i == 3 or i == 5:
+        continue
+    print(f"Produto {i}")
